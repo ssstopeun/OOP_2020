c9a312dbff5965dbacad13990a6b18fe16da30c6
jeongji1416@naver.com <jeongji1416@naver.com>
2020-09-27 03:25:15 +0900

    inti

--- OOP_Homework04/OOP_Homework04/Homework01.c
+++ OOP_Homework04/OOP_Homework04/Homework01.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	char s[100];
-	prinf("문자를 입력하세요(100자 이내).");
-	scamf("%s", &s);
-	printf("입력된 문자는 &s 입니다.");
-
+	char string[101];
+	printf("문자를 입력하세요(100자 이내).");
+	scanf("%s", string);
+	printf("입력된 문자는 %s 입니다.", string);
 	return 0;
 }
